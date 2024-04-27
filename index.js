badb6da7b403a85089e21c5527ae462e5a8d2d81
codeamin11 <legacyofalamin@gmail.com>
2024-04-28 01:45:48 +0600

    post method implemented

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyujvjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,10 +25,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const artsCollection = client.db('artsDB').collection('arts')
+
+
+
+    // create data
+    app.post('/addCrafts', async(req, res)=>{
+      const newArts = req.body;
+      console.log(newArts);
+      const result = await artsCollection.insertOne(newArts)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
