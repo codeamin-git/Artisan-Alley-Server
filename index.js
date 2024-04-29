1fcf7aca8ca34610f4b2c2e2d0880df516acab2c
codeamin11 <legacyofalamin@gmail.com>
2024-04-29 17:12:48 +0600

    delete method implemented

--- index.js
+++ index.js
@@ -113,6 +113,13 @@ async function run() {
       const result = await artsCollection.updateOne(query, data)
       res.send(result)
     })
+
+    // delete data
+    app.delete('/deleteItem/:id', async(req, res)=>{
+      const result = await artsCollection.deleteOne({_id: new ObjectId(req.params.id)})
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
